8b4c74bf278e5725448eb21d23e6baa1496eab19
MaxJiang <maxjiang93@gmail.com>
2017-04-19 16:18:56 -0700

    update

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import tensorflow as tf
 
 flags = tf.app.flags
 flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
-flags.DEFINE_float("d_learning_rate", 0.0025, "Learning rate of discrim for adam [0.00005]")
-flags.DEFINE_float("g_learning_rate", 0.0025, "Learning rate of gen for adam [0.0025]")
+flags.DEFINE_float("d_learning_rate", 0.0002, "Learning rate of discrim for adam [0.00005]")
+flags.DEFINE_float("g_learning_rate", 0.0005, "Learning rate of gen for adam [0.0025]")
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
 flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
 flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
